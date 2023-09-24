bc12bed5153333906d88bcfa9bca447eba270e7e
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-09-24 14:31:03 +0100

    set up framework - code flow, methods..

--- src/songapplication/SongApplication.java
+++ src/songapplication/SongApplication.java
@@ -1,20 +1,90 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package songapplication;
-
 /**
- *
- * @author adunderdale
+ * initialSongProject.java
+ * 
+ * This file contains the main class for the song application.
+ * 
+ * @author Ashton Dunderdale
+ * Date: September 24, 2023 
+ * 
  */
-public class SongApplication {
 
-    /**
-     * @param args the command line arguments
-     */
+package songapplication;
+import java.util.Scanner;
+
+public class SongApplication {
+    
+    // Scanner for user input
+    static Scanner userInput = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // calls initial method
+        processMenuChoice(); 
     }
     
+    
+    /**
+     * Process the user menu choice in a loop.
+     */
+    public static void processMenuChoice(){
+    
+    }
+    
+
+     /**
+     * Get the user menu choice and validate it
+     * 
+     * @return the valid menu choice.
+     */  
+    public static int getUserMenuChoice(){
+    
+    }
+    
+    
+     /**
+     * Display the menu to the user
+     */
+    public static void displayMenu(){ // recursive
+        System.out.println("""
+                   \tMusic Player\n
+                   Choose a menu option:\n
+                   \t1. Add Song
+                   \t2. Remove Song
+                   \t3. View Songs
+                   \t4. View Top Songs
+                   \n\t0. Exit Application
+                   """);
+    }
+    
+    
+     /**
+     * Adds a chosen song, generates random playCount
+     */   
+    public static void addSong(){
+    
+    }
+    
+    
+     /**
+     * Removes a chosen song 
+     */   
+    public static void removeSong(){
+    
+    }
+    
+    
+     /**
+     * Shows a list of all songs
+     */   
+    public static void viewSongs(){
+    
+    }
+    
+    
+     /**
+     * Shows the top ten songs in order of play count
+     */   
+    public static void viewTopSongs(){
+    
+    }
 }
